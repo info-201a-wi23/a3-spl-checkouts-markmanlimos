5a7ddecdeeda6f138cb1448c9a73307f30959799
Mark <mxpm02@uw.edu>
2023-02-22 12:58:53 -0800

    finished chart 2

--- chart2_example.R
+++ chart2_example.R
@@ -1,6 +1,32 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
+library(dplyr)
+library(stringr)
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+library(scales)
+# load in data
+spl_df <- read.csv("2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+# Creating date column that has month and year with default first day of month
+# then converts them to date
+spl_df <- spl_df %>% mutate(date = as.Date(paste0(CheckoutYear, '-', CheckoutMonth,'-01'),'%Y-%m-%d'))
+
+# Purpose of chart: compare checkout rates of 3 checkout types (Book, Ebook, and Audiobook) over months in 2022
+
+# filter for the 3 book types and other relevant data
+
+material_checkouts <- spl_df %>%
+  filter(CheckoutYear == 2022) %>%
+  filter(MaterialType %in% c("BOOK", "AUDIOBOOK", "EBOOK")) %>%
+  group_by(CheckoutMonth, MaterialType) %>%
+  summarize(material_total = sum(Checkouts))
+
+# Creating the Plot
+
+ggplot(data = material_checkouts, aes(fill = MaterialType, y = material_total, x = CheckoutMonth)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  labs(
+    title = "Comparing Medium of Checkouts by Month in 2022",
+    x = "Checkout Month",
+    y = "Number of Checkouts"
+  ) +
+  scale_x_continuous(breaks = seq(1,12,1)) +
+  scale_y_continuous(labels = label_number_si())
